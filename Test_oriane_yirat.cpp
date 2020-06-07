fcbfcdf0c8f4d8fafd5bbea0fa94800aecf8b3c2
Shani Hayik <shanihayik@gmail.com>
2020-06-07 17:50:15 +0300

    Add files via upload

--- Test_oriane_yirat.cpp
+++ Test_oriane_yirat.cpp
@@ -148,33 +148,33 @@ TEST_CASE("Move Function - Commander"){
             CHECK(typeid(*p[{3,4}])==typeid(ParamedicCommander));
 }
 
-//TEST_CASE("Exceptions") {
-//    WarGame::Board p = fillBoard();
-//    //Out of board
-//            CHECK_THROWS(p.move(1, {5, 1}, WarGame::Board::Up));
-//            CHECK_THROWS(p.move(1, {0, 0}, WarGame::Board::Down));
-//            CHECK_THROWS(p.move(2, {3, 5}, WarGame::Board::Right));
-//            CHECK_THROWS(p.move(1, {4, 5}, WarGame::Board::Right));
-//            CHECK_THROWS(p.move(1, {5, 0}, WarGame::Board::Left));
-//    //Busy place
-//            CHECK_THROWS(p.move(2, {4, 0}, WarGame::Board::Up));
-//            CHECK_THROWS(p.move(2, {3, 0}, WarGame::Board::Up));
-//            CHECK_THROWS(p.move(2, {0, 3}, WarGame::Board::Up));
-//            CHECK_THROWS(p.move(2, {1, 3}, WarGame::Board::Down));
-//            CHECK_THROWS(p.move(1, {0, 1}, WarGame::Board::Left));
-//    //Null Place
-//            CHECK_THROWS(p.move(1, {2, 0}, WarGame::Board::Up));
-//            CHECK_THROWS(p.move(1, {4, 3}, WarGame::Board::Up));
-//            CHECK_THROWS(p.move(1, {2, 2}, WarGame::Board::Down));
-//            CHECK_THROWS(p.move(2, {1, 0}, WarGame::Board::Right));
-//            CHECK_THROWS(p.move(2, {5, 4}, WarGame::Board::Left));
-//    //Source out of bound
-//            CHECK_THROWS(p.move(1, {6, 0}, WarGame::Board::Up));
-//            CHECK_THROWS(p.move(1, {7, 3}, WarGame::Board::Up));
-//            CHECK_THROWS(p.move(1, {8, 2}, WarGame::Board::Down));
-//            CHECK_THROWS(p.move(2, {9, 0}, WarGame::Board::Right));
-//            CHECK_THROWS(p.move(2, {10, 4}, WarGame::Board::Left));
-//}
+TEST_CASE("Exceptions") {
+    WarGame::Board p = fillBoard();
+    //Out of board
+            CHECK_THROWS(p.move(1, {5, 1}, WarGame::Board::Up));
+            CHECK_THROWS(p.move(1, {0, 0}, WarGame::Board::Down));
+            CHECK_THROWS(p.move(2, {3, 5}, WarGame::Board::Right));
+            CHECK_THROWS(p.move(1, {4, 5}, WarGame::Board::Right));
+            CHECK_THROWS(p.move(1, {5, 0}, WarGame::Board::Left));
+    //Busy place
+            CHECK_THROWS(p.move(2, {4, 0}, WarGame::Board::Up));
+            CHECK_THROWS(p.move(2, {3, 0}, WarGame::Board::Up));
+            CHECK_THROWS(p.move(2, {0, 3}, WarGame::Board::Up));
+            CHECK_THROWS(p.move(2, {1, 3}, WarGame::Board::Down));
+            CHECK_THROWS(p.move(1, {0, 1}, WarGame::Board::Left));
+    //Null Place
+            CHECK_THROWS(p.move(1, {2, 0}, WarGame::Board::Up));
+            CHECK_THROWS(p.move(1, {4, 3}, WarGame::Board::Up));
+            CHECK_THROWS(p.move(1, {2, 2}, WarGame::Board::Down));
+            CHECK_THROWS(p.move(2, {1, 0}, WarGame::Board::Right));
+            CHECK_THROWS(p.move(2, {5, 4}, WarGame::Board::Left));
+    //Source out of bound
+            CHECK_THROWS(p.move(1, {6, 0}, WarGame::Board::Up));
+            CHECK_THROWS(p.move(1, {7, 3}, WarGame::Board::Up));
+            CHECK_THROWS(p.move(1, {8, 2}, WarGame::Board::Down));
+            CHECK_THROWS(p.move(2, {9, 0}, WarGame::Board::Right));
+            CHECK_THROWS(p.move(2, {10, 4}, WarGame::Board::Left));
+}
 
 TEST_CASE("Check Moves"){
     WarGame::Board p = fillBoard();
